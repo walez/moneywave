67b3e0e3681a7314c880b3cb384a9dc38014865c
Jonathan Itakpe <jonathanitakp@gmail.com>
2017-05-31 22:43:56 +0100

    Forgot Comma

--- __tests__/moneywave.test.ts
+++ __tests__/moneywave.test.ts
@@ -4,8 +4,8 @@ import Config from './config';
 describe("Moneywave", () => {
     test("cache is not null", () => {
         let moneywave = new Moneywave({
-            apiKey: Config.apiKey
-            apiSecret: Config.apiSecret
+            apiKey: Config.apiKey,
+            apiSecret: Config.apiSecret,
             cache: true
         });
         expect(moneywave.cache).not.toBeNull();
