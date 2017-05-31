419eeef3942944186861c6663ff46f3f94c51362
Jonathan Itakpe <jonathanitakp@gmail.com>
2017-05-31 22:42:09 +0100

    Extracted api details

---
+++ __tests__/config.ts
@@ -0,0 +1,4 @@
+export let config = {
+  apiKey: '',
+  apiSecret: ''
+}
--- __tests__/moneywave.test.ts
+++ __tests__/moneywave.test.ts
@@ -1,10 +1,11 @@
 import Moneywave from '../src/Moneywave';
+import Config from './config';
 
 describe("Moneywave", () => {
     test("cache is not null", () => {
         let moneywave = new Moneywave({
-            apiKey: "",
-            apiSecret: "",
+            apiKey: Config.apiKey
+            apiSecret: Config.apiSecret
             cache: true
         });
         expect(moneywave.cache).not.toBeNull();
